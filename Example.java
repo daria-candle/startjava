90da9ea4c117bceab06a5c1e2459486803ddd0f5
daria-candle <darja.svechckina@yandex.ru>
2025-12-04 16:15:15 +0300

    Инициализация проекта
    - добавила файл с описанием проекта
    - добавила реализацию первых трех классов

---
+++ Example.java
@@ -0,0 +1,10 @@
+/*
+Это пример простой программы на Java .
+Присвойте файлу с исходным кодом имя Example.java .
+*/
+class Example {
+    // Выполнение программы на Java начинается с вызова метода main ()
+    public static void main(String[]args) {
+        System.out.println(" Java правит Интернетом!");
+    }
+}
---
+++ VariablesTheme.java
@@ -0,0 +1,8 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+        System.out.println("1 Создание переменных и вывод их значений на консоль");
+        byte number_of_cores = 2;
+        System.out.println("");
+    }
+}
+
