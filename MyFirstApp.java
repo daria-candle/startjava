5365d6d8de12421cadb2e44347bc91174bd3efef
daria-candle <darja.svechckina@yandex.ru>
2026-01-16 12:42:55 +0300

    Изменила вывод текста, отображаемого в консоль

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class MyFirstApp {
     public static void main(String[]args) {
-        System.out.println(" Написано однажды, работает везде ");
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде!");
         Scanner console = new Scanner(System.in, "cp866");
         System.out.print("Введите свое имя: ");
         System.out.println(console.nextLine());
