97796dd451815289fee70c34fafd1162fdb8820f
daria-candle <darja.svechckina@yandex.ru>
2026-02-02 10:25:19 +0300

    Добавила IfElseStatementTheme.java, VariableNamesTheme.java, VariablesTheme.java

---
+++ IfElseStatementTheme.java
@@ -0,0 +1,189 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class IfElseStatementTheme {
+    public static void main(String[] args) {
+        System.out.println("1. ПЕРЕВОД ПСЕВДОКОДА НА ЯЗЫК JAVA\n");
+        boolean isGenderMale = false;
+        if (!isGenderMale) {
+            System.out.println("Вы - мужчина");
+        } else {
+            System.out.println("Вы - женщина!");
+        }
+        byte age = 17;
+        if (age > 18) {
+            System.out.println("Возможно, вы - Юрий Лоза");
+        } else {
+            System.out.println("Вы точно не Юрий Лоза!");
+        }
+        float height = 1.7f;
+        if (height < 1.8) {
+            System.out.println("Вы ниже Дэвида Бекхэма");
+        } else {
+            System.out.println("Вы как Дэвид Бекхэм или даже выше!");
+        }
+
+        System.out.println("\n\n2. ПОИСК БОЛЬШЕГО ЧИСЛА ШАГОВ\n");
+        int stepCountYesterday = 21062;
+        int stepCountToday = 10763;
+        System.out.println("Пройдено вчера (в шагах): " + stepCountYesterday);
+        System.out.println("Пройдено сегодня (в шагах): " + stepCountToday);
+        if (stepCountYesterday > stepCountToday) {
+            System.out.println("Вчера вы прошли больше, чем сегодня!");
+        } else if (stepCountYesterday < stepCountToday) {
+            System.out.println("Вчера вы прошли меньше, чем сегодня!");
+        } else {
+            System.out.println("Вчера вы прошли столько же, как сегодня!");
+        }
+        double stepCountAverage = (stepCountYesterday + stepCountToday) / 2.0;
+        System.out.println("Среднее количество шагов за 2 дня: " + stepCountAverage);
+
+        System.out.println("\n\n3. ПРОВЕРКА КОЛИЧЕСТВА ГОСТЕЙ");
+        int guestsNumber = 15;
+        if (guestsNumber == 0) {
+            System.out.println("Пока никто не записался на мероприятие!");
+        } else if (guestsNumber < 0) {
+            System.out.println("Ошибка! Количество гостей отрицательное!");
+        } else if ((guestsNumber % 2) == 0) {
+            System.out.printf("Записалось %d гостей. Можно формировать пары для конкурсов.%n", guestsNumber);
+        } else {
+            System.out.printf("Записалось %d гостей. Нужны индивидуальные задания.%n", guestsNumber);
+        }
+
+        System.out.println("\n\n4. ОПРЕДЕЛЕНИЕ ПЕРВОГО СИМВОЛА НИКНЕЙМА\n");
+        String nickname = "daria_candle";
+        char nicknameFirstLetter = nickname.charAt(0);
+        System.out.println("Первый способ решения:");
+        if ((nicknameFirstLetter >= 'a' && nicknameFirstLetter <= 'z') | 
+                (nicknameFirstLetter >= 'а' && nicknameFirstLetter <= 'я')) {
+            System.out.println("Имя " + nickname + " начинается с маленькой буквы '" + 
+                    nicknameFirstLetter + "'");
+        } else if ((nicknameFirstLetter >= 'A' && nicknameFirstLetter <= 'Z') | 
+                (nicknameFirstLetter >= 'А' && nicknameFirstLetter <= 'Я')) {
+            System.out.println("Имя " + nickname + " начинается с большой буквы '" + 
+                    nicknameFirstLetter + "'");
+        } else if (nicknameFirstLetter >= '0' && nicknameFirstLetter <= '9') {
+            System.out.println("Имя " + nickname + " начинается с цифры '" + nicknameFirstLetter + "'");
+        } else {
+            System.out.println("Имя " + nickname + " начинается с символа '" + nicknameFirstLetter + "'");
+        }
+        System.out.println("\nВторой способ решения:");
+        if (Character.isLowerCase(nicknameFirstLetter) == true) {
+            System.out.println("Имя " + nickname + " начинается с маленькой буквы '" + 
+                    nicknameFirstLetter + "'");
+        } else if (Character.isUpperCase(nicknameFirstLetter) == true) {
+            System.out.println("Имя " + nickname + " начинается с большой буквы '" + 
+                    nicknameFirstLetter + "'");
+        } else if (Character.isDigit(nicknameFirstLetter) == true) {
+            System.out.println("Имя " + nickname + " начинается с цифры '" + nicknameFirstLetter + "'");
+        } else {
+            System.out.println("Имя " + nickname + " начинается с символа '" + nicknameFirstLetter + "'");
+        }
+
+        System.out.println("\n\n5. ИНВЕНТАРИЗАЦИЯ\n");
+        short dbId = 123;
+        short currId = 103;
+        char dbIdHundreds = String.valueOf(dbId).charAt(0);
+        char dbIdTens = String.valueOf(dbId).charAt(1);
+        char dbIdOnes = String.valueOf(dbId).charAt(2);
+        char currIdHundreds = String.valueOf(currId).charAt(0);
+        char currIdTens = String.valueOf(currId).charAt(1);
+        char currIdOnes = String.valueOf(currId).charAt(2);
+        if (dbId == currId) {
+            System.out.println("[№" + currId + "]: компьютер на 3-м этаже в кабинете 2");
+        } else if (dbIdHundreds != currIdHundreds && dbIdTens != currIdTens &&
+                dbIdOnes != currIdOnes) {
+            System.out.println("[№" + currId + "]: оборудование не идентифицировано");
+        } else {
+            System.out.printf("""
+                    Нет полного совпадения:
+                    База данных: [№%d]
+                    Фактический: [%s%s%s]%n
+                    """, dbId, dbIdHundreds == currIdHundreds ? dbIdHundreds : "_", 
+                    dbIdTens == currIdTens ? dbIdTens : "_",
+                    dbIdOnes == currIdOnes ? dbIdOnes : "_");
+        }
+
+        System.out.println("\n\n6. ПОДСЧЕТ НАЧИСЛЕННЫХ БАНКОМ %\n");
+        System.out.println("первый способ решения (без округления):");
+        float depositBalance = 321123.79f;
+        float depositRate;
+        if (depositBalance <= 0.0f) {
+            System.out.println("Внимание! Баланс отрицательный");
+            depositRate = 0.0f;
+        } else if (depositBalance < 100000.0f) {
+            depositRate = 0.05f;
+        } else if (depositBalance <= 300000.0f) {
+            depositRate = 0.07f;
+        } else {
+            depositRate = 0.1f;
+        }
+        float depositRateSum = depositBalance * depositRate;
+        float depositCommonSum = depositBalance + depositRateSum;
+        System.out.println("сумма вклада: " + depositBalance + " руб.");
+        System.out.println("сумма начисленного %: " + depositRateSum + " руб.");
+        System.out.println("итоговая сумма с %: " + depositCommonSum + " руб.");
+        System.out.println("\nвторой способ решения (с округлением):");
+        BigDecimal depositBalanceBd = BigDecimal.valueOf(321123.79);
+        BigDecimal depositRateBd;
+        if (depositBalanceBd.floatValue() <= 0.0f) {
+            System.out.println("Внимание! Баланс отрицательный");
+            depositRateBd = BigDecimal.valueOf(0.0);
+        } else if (depositBalanceBd.floatValue() < 100000.0f) {
+            depositRateBd = BigDecimal.valueOf(0.05);
+        } else if (depositBalanceBd.floatValue() <= 300000.0f) {
+            depositRateBd = BigDecimal.valueOf(0.07);
+        } else {
+            depositRateBd = BigDecimal.valueOf(0.1);
+        }
+        BigDecimal depositRateSumBd =
+                depositBalanceBd.multiply(depositRateBd).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal depositCommonSumBd = depositBalanceBd.add(depositRateSumBd);
+        System.out.println("сумма вклада: " + depositBalanceBd + " руб.");
+        System.out.println("сумма начисленного %: " + depositRateSumBd + " руб.");
+        System.out.println("итоговая сумма с %: " + depositCommonSumBd + " руб.");
+
+        System.out.println("\n\n7. ОПРЕДЕЛЕНИЕ ОЦЕНКИ ПО ПРЕДМЕТАМ %\n");
+        byte historyResult = 59;
+        byte historyGrade;
+        if (historyResult <= 60) {
+            historyGrade = 2;
+        } else if (historyResult <= 73) {
+            historyGrade = 3;
+        } else if (historyResult <= 90) {
+            historyGrade = 4;
+        } else {
+            historyGrade = 5;
+        }
+        byte codingResult = 92;
+        byte codingGrade;
+        if (codingResult <= 60) {
+            codingGrade = 2;
+        } else if (codingResult <= 73) {
+            codingGrade = 3;
+        } else if (codingResult <= 90) {
+            codingGrade = 4;
+        } else {
+            codingGrade = 5;
+        }
+        float gradeAverage = (historyGrade + codingGrade) / 2.0f;
+        float resultsAverage = (historyResult + codingResult) / 2.0f;
+        System.out.println("Оценка по истории: " + historyGrade);
+        System.out.println("Оценка по программированию: " + codingGrade);
+        System.out.println("Средний балл оценок по предметам: " + gradeAverage);
+        System.out.println("Средний % по предметам: " + resultsAverage);
+
+        System.out.println("\n\n8. РАСЧЕТ ГОДОВОЙ ПРИБЫЛИ %\n");
+        BigDecimal revenueMonth = BigDecimal.valueOf(13025.233);
+        BigDecimal rentMonth = BigDecimal.valueOf(5123.018);
+        BigDecimal costPriceMonth = BigDecimal.valueOf(9001.729);
+        BigDecimal monthsCount = BigDecimal.valueOf(12);
+        BigDecimal profitMonth = revenueMonth.subtract(rentMonth).subtract(costPriceMonth);
+        BigDecimal profitYear = profitMonth.multiply(monthsCount).setScale(2, RoundingMode.HALF_UP);
+        if (profitYear.signum() == 1) {
+            System.out.println("Прибыль за год: +" + profitYear + " руб.");
+        } else {
+            System.out.println("Прибыль за год: " + profitYear + " руб.");
+        }
+    }
+}
--- VariablesTheme.java
+++ VariablesTheme.java
@@ -1,8 +1,183 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalTime;
+
 public class VariablesTheme {
     public static void main(String[] args) {
-        System.out.println("1 Создание переменных и вывод их значений на консоль");
-        byte number_of_cores = 2;
-        System.out.println("");
+        final LocalTime localTimeStart = LocalTime.now();
+        final long timeStart = System.nanoTime();
+        System.out.println("1 ВЫВОД ASCII-ГРАФИКИ");
+        System.out.println("\nс помощью String.join():");
+        System.out.println(String.join(" ","                    ","/\\"));
+        System.out.println(String.join(" ","   J","   a"," v     v"," /  \\"));
+        System.out.println(String.join(" ","   J","  a a"," v   v"," /_( )\\"));
+        System.out.println(String.join(" ","J  J"," aaaaa"," V V"," /      \\"));
+        System.out.println(String.join(" "," JJ"," a     a"," V"," /___/\\___\\"));
+        System.out.println("\nс помощью текстового блока:");
+        System.out.println("""
+                         /\\            
+                   J    /  \\  v     v  a
+                   J   /_( )\\  v   v  a a
+                J  J  /      \\  V V  aaaaa
+                 JJ  /___/\\___\\  V  a     a
+                 """);
+
+        System.out.println("\n2 РАСЧЕТ СТОИМОСТИ ТОВАРА");
+        System.out.println("\nпервый способ решения (без округления):");
+        float penCost = 105.5f;
+        float bookCost = 235.23f;
+        float discont = 0.11f;
+        float productsSumFull = penCost + bookCost;
+        float discountSum = productsSumFull * discont;
+        float productsSumWithDiscount = productsSumFull - discountSum;
+        System.out.println("стоимость товаров без скидки: " + productsSumFull + " руб.");
+        System.out.println("сумма скидки: " + discountSum + " руб.");
+        System.out.println("стоимость товаров со скидкой: " + productsSumWithDiscount + " руб.");
+        System.out.println("\nвторой способ решения (с округлением):");
+        BigDecimal penCostBd = BigDecimal.valueOf(105.5);
+        BigDecimal bookCostBd = BigDecimal.valueOf(235.23);
+        BigDecimal discontBd = BigDecimal.valueOf(0.11);
+        BigDecimal productsSumFullBd = penCostBd.add(bookCostBd);
+        BigDecimal discountSumBd = discontBd.multiply(productsSumFullBd).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal productsSumWithDiscountBd = productsSumFullBd.subtract(discountSumBd);
+        System.out.println("стоимость товаров без скидки: " + productsSumFullBd + " руб.");
+        System.out.println("сумма скидки: " + discountSumBd + " руб.");
+        System.out.println("стоимость товаров со скидкой: " + productsSumWithDiscountBd + " руб.\n");
+
+        System.out.println("\n3. ПЕРЕСТАНОВКА ЗНАЧЕНИЙ ЯЧЕЕК В ТАБЛИЦЕ\n");
+        int a1 = 2;
+        int b1 = 5;
+        System.out.println("Исходные значения переменных: A1 = " + a1 + ", B1 = " + b1);
+        System.out.println("\nМетод: третья переменная");
+        int c1 = a1;
+        a1 = b1;
+        b1 = c1;
+        System.out.println("Результат: A1 = " + a1 + ", B1 = " + b1);
+        System.out.println("\nМетод: арифметические операции");
+        a1 = 2;
+        b1 = 5;
+        a1 += b1;
+        b1 = a1 - b1;
+        a1 -= b1;
+        System.out.println("Результат: A1 = " + a1 + ", B1 = " + b1);
+        System.out.println("\nМетод: побитовый");
+        a1 = 2;
+        b1 = 5;
+        a1 ^= b1;
+        b1 ^= a1;
+        a1 ^= b1;
+        System.out.println("Результат: A1 = " + a1 + ", B1 = " + b1 + "\n");
+
+        System.out.println("\n4. ДЕКОДИРОВАНИЕ СООБЩЕНИЯ\n");
+        int asciiCode1 = 1055;
+        int asciiCode2 = 1088;
+        int asciiCode3 = 1080;
+        int asciiCode4 = 1074;
+        int asciiCode5 = 1077;
+        int asciiCode6 = 1090;
+        char character1 = (char) asciiCode1;
+        char character2 = (char) asciiCode2;
+        char character3 = (char) asciiCode3;
+        char character4 = (char) asciiCode4;
+        char character5 = (char) asciiCode5;
+        char character6 = (char) asciiCode6;
+        System.out.printf("%5d%5d%5d%5d%5d%5d%n", asciiCode1, asciiCode2, asciiCode3, 
+                asciiCode4, asciiCode5, asciiCode6);
+        System.out.printf("%5s%5s%5s%5s%5s%5s%n%n", character1, character2, character3, 
+                character4, character5, character6);
+
+        System.out.println("\n5. АНАЛИЗ КОДА ТОВАРА\n");
+        int productCode = 123;
+        int productGroup = productCode / 100;
+        int productSubgroup = (productCode % 100) / 10;
+        int packingType = productCode % 10;
+        int controlSum = productGroup + productSubgroup + packingType;
+        int checkCode = productGroup * productSubgroup * packingType;
+        System.out.printf("""
+                Код товара: %d
+                  категория товара - %d
+                  подкатегория - %d
+                  тип упаковки - %d
+                Контрольная сумма = %d
+                Проверочный код = %d%n
+                """, productCode, productGroup, productSubgroup, packingType, controlSum, checkCode);
+
+        System.out.println("\n6. ТЕСТИРОВАНИЕ ДАТЧИКОВ ПЕРЕД ЗАПУСКОМ РАКЕТЫ\n");
+        byte temperature = Byte.MAX_VALUE;
+        System.out.printf("""
+                [Температура, °C]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d)
+                """, temperature, ++temperature, --temperature);
+        short pressure = Short.MAX_VALUE;
+        System.out.printf("""
+                [Давление, Па]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d)
+                """, pressure, ++pressure, --pressure);
+        char systemStatusCode = Character.MAX_VALUE;
+        System.out.printf("""
+                [Код состояния системы]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d)
+                """, (int) systemStatusCode, (int) ++systemStatusCode, (int) --systemStatusCode);
+        int mileage = Integer.MAX_VALUE;
+        System.out.printf("""
+                [Пройденное расстояние, км]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d)
+                """, mileage, ++mileage, --mileage);
+        long startTime = Long.MAX_VALUE;
+        System.out.printf("""
+                [Время с момента старта, сек]:
+                  Исходное: %d
+                  +1: %d
+                  -1: %d)
+                %n""", startTime, ++startTime, --startTime);
+
+        System.out.println("\n7. ВЫВОД ПАРАМЕТРОВ JVM И ОС\n");
+        System.out.println(" Характеристики JVM");
+        Runtime runtime = Runtime.getRuntime();
+        int availableProcessors = runtime.availableProcessors();
+        double totalMemory = runtime.totalMemory() / 1000000;
+        double freeMemory = runtime.freeMemory() / 1000000;
+        double usedMemory = totalMemory - freeMemory;
+        double maxMemory = runtime.maxMemory() / 1000000;
+        System.out.printf("""
+                 - доступное число ядер: %d
+                 - выделенная память (МБ): %.1f
+                 - свободная память (МБ): %.1f
+                 - используемая память (МБ): %.1f
+                 - максимально доступная для выделения память (МБ): %.1f
+                """, availableProcessors, totalMemory, freeMemory, usedMemory, maxMemory);
+
+        System.out.println("\n Параметры ОС");
+        char systemDiskLetter = System.getProperty("user.home").charAt(0);
+        String osVersion = System.getProperty("os.version");
+        String javaVersion = System.getProperty("java.version");
+        char fileSeparator = System.getProperty("file.separator").charAt(0);
+        System.out.printf("""
+                 - системный диск (буква): %s
+                 - версия ОС: %s
+                 - версия Java: %s
+                 - символ разделения пути (сепаратор): %s
+                %n""", systemDiskLetter, osVersion, javaVersion, fileSeparator);
+
+        System.out.println("\n8. ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА\n");
+        long timeFinish = System.nanoTime();
+        LocalTime localTimeFinish = LocalTime.now();
+        double timeElapsed = (timeFinish - timeStart) / 1000000000.0;
+        System.out.printf("""
+                | Старт проверки | %1$tH:%1$tM:%1$tS.%1$tL |
+                +----------------+--------------+
+                | Финиш проверки | %2$tH:%2$tM:%2$tS.%2$tL |
+                +----------------+--------------+
+                | Время работы   | %3$.3f сек    |
+                %n""", localTimeStart, localTimeFinish, timeElapsed);
     }
 }
 
