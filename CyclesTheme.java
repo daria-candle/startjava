decb2a6a708ddac835941bb4afe2c0bef637954c
daria-candle <darja.svechckina@yandex.ru>
2026-02-07 21:34:52 +0300

    Внесла правки после проверки в CyclesTheme.java

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -5,16 +5,15 @@ public class CyclesTheme {
         System.out.println("\n1. ВЫВОД ASCII-СИМВОЛОВ\n");
         System.out.printf("%s%12s%14s%n", "DECIMAL", "CHARACTER", "DESCRIPTION");
         for (int asciiCode = 33; asciiCode <= 122; asciiCode += 2) {
-            if (asciiCode <= 47 | asciiCode >= 98) {
-                System.out.printf("%4d%11c%12s%-1s%n", asciiCode, asciiCode, "", 
-                        Character.getName(asciiCode));
+            if (asciiCode == 49) {
+                asciiCode = 98;
             }
+            System.out.printf("%4d%11c%12s%-1s%n", asciiCode, asciiCode, "", Character.getName(asciiCode));
         }
 
         System.out.println("\n\n2. ВЫВОД ГЕОМЕТРИЧЕСКИХ ФИГУР\n");
         for (int row = 1; row <= 5; row++) {
-            System.out.print("----------");
-            System.out.print(" ");
+            System.out.print("---------- ");
             for (int col = 1; col <= (6 - row); col++) {
                 System.out.print("*");
             }
@@ -70,40 +69,46 @@ public class CyclesTheme {
         }
 
         System.out.println("\n\n\n6. РАЗНЫЕ ОПЕРАЦИИ НАД ЧИСЛОМ\n");
-        originalNumber = 2234321;
+        originalNumber = 1234321;
+        System.out.println("Исходное число: " + originalNumber);
         int twosCount = 0;
         boolean isPalindrome = true;
-        System.out.println("Исходное число: " + originalNumber);
-        for (int divider = 1; divider <= 1000000; divider *= 10) {
-            int rightNumber = originalNumber % (divider * 10) / divider;
-            int leftNumber = originalNumber % (10000000 / divider) / (1000000 / divider);
-            System.out.print(rightNumber);
-            twosCount = (rightNumber == 2) ? twosCount + 1 : twosCount;
-            isPalindrome = (rightNumber != leftNumber) ? false : isPalindrome;
+        int reversedNumber = originalNumber % 10;
+        int currentNumber = originalNumber / 10;
+        while (currentNumber > 0) {
+            reversedNumber *= 10;
+            int digit = currentNumber % 10;
+            reversedNumber += digit;
+            if (digit == 2) {
+                twosCount++;
+            }
+            currentNumber /= 10;
         }
-        System.out.printf(" - %s с %s (%d) количеством двоек %n", 
+        isPalindrome = (originalNumber != reversedNumber) ? false : isPalindrome;
+        System.out.printf("%d - %s с %s (%d) количеством двоек %n",
+                reversedNumber,
                 (isPalindrome ? "палиндром" : "не палиндром"),
                 ((twosCount % 2.0 == 0) ? "четным" : "нечетным"),
                 twosCount);
     
         System.out.println("\n\n7. ПРОВЕРКА СЧАСТЛИВОГО ЧИСЛА\n");
         originalNumber = 101002;
-        int leftNumber = originalNumber / 1000;
-        int rightNumber = originalNumber % 1000;
-        int leftSum = 0;
-        int rightSum = 0;
-        int divider = 1;
-        do {
-            leftSum = leftSum + (leftNumber % (divider * 10) / divider);
-            rightSum = rightSum + (rightNumber % (divider * 10) / divider);
-            divider *= 10;
-        } while (divider <= 100);
+        int leftHalf = originalNumber / 1000;
+        int rightHalf = originalNumber % 1000;
+        int leftSum = leftHalf % 10;
+        int rightSum = rightHalf % 10;
+        while (leftHalf > 0) {
+            leftHalf /= 10;
+            rightHalf /= 10;
+            leftSum += leftHalf % 10;
+            rightSum += rightHalf % 10;
+        }
         System.out.printf("""
                 %d - %s число
                 Сумма цифр %03d = %d
                 Сумма цифр %03d = %d
                 """, originalNumber, (leftSum == rightSum) ? "счастливое" : "не счастливое",
-                leftNumber, leftSum, rightNumber, rightSum);
+                originalNumber / 1000, leftSum, originalNumber % 1000, rightSum);
 
         System.out.println("\n\n8. ПРОСТОЙ ГЕНЕРАТОР ПАРОЛЯ\n");
         String password = "";
@@ -114,7 +119,7 @@ public class CyclesTheme {
         boolean hasDigit = false;
         for (int i = 1; i <= 8; i++) {
             char character = (char) new Random().nextInt(33, 126);
-            password = password + character;
+            password += character;
             if (character > 96 && character < 123) {
                 hasLowerCase = true;
             } else if (character > 64 && character < 91) {
