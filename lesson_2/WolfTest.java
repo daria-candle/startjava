a2ecde1e2d60c7d691abb563f41b9b8b3ac22013
daria-candle <darja.svechckina@yandex.ru>
2026-02-22 18:20:09 +0300

    Добавлен класс WolfTest.java

--- lesson_2/WolfTest.java
+++ lesson_2/WolfTest.java
@@ -1,6 +1,20 @@
 public class WolfTest {
     public static void main(String[] args) {
-        Wolf wolf;
-        
+        Wolf wolf = new Wolf();
+        wolf.gender = 'м';
+        wolf.name = "Акела";
+        wolf.weight = 43.2f;
+        wolf.age = 10;
+        wolf.color = "серый";
+        System.out.println("пол: " + wolf.gender);
+        System.out.println("кличка: " + wolf.name);
+        System.out.println("вес: " + wolf.weight);
+        System.out.println("возраст: " + wolf.age);
+        System.out.println("окрас: " + wolf.color);
+        wolf.go();
+        wolf.sit();
+        wolf.run();
+        wolf.howl();
+        wolf.hunt();
     }
 }
