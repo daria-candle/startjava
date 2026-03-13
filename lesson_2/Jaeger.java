344b0c8fd48459b80fdfc5ec44e91d57f8cb178d
daria-candle <darja.svechckina@yandex.ru>
2026-03-13 14:00:02 +0300

    Добавлены классы Jaeger, JaegerTest

---
+++ lesson_2/Jaeger.java
@@ -0,0 +1,133 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public Jaeger() {
+    }
+
+    public Jaeger(String modelName, String mark, String origin, float height, 
+            float weight, int speed, int strength, int armor) {
+        setModelName(modelName);
+        setMark(mark);
+        setOrigin(origin);
+        setHeight(height);
+        setWeight(weight);
+        setSpeed(speed);
+        setStrength(strength);
+        setArmor(armor);
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    void setModelName(String modelName) {
+        if (modelName == "") {
+            System.out.println("Пустая строка");
+        } else {
+            this.modelName = modelName;
+        }
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    void setMark(String mark) {
+        if (mark == "") {
+            System.out.println("Пустая строка");
+        } else {
+            this.mark = mark;
+        }
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    void setOrigin(String origin) {
+        if (origin == "") {
+            System.out.println("Пустая строка");
+        } else {
+            this.origin = origin;
+        }
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    void setHeight(float height) {
+        if (height <= 0.0) {
+            System.out.println("Некорректный ввод");
+        } else {
+            this.height = height;
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    void setWeight(float weight) {
+        if (weight <= 0.0) {
+            System.out.println("Некорректный ввод");
+        } else {
+            this.weight = weight;
+        }
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    void setSpeed(int speed) {
+        if (speed <= 0) {
+            System.out.println("Некорректный ввод");
+        } else {
+            this.speed = speed;
+        }
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    void setStrength(int strength) {
+        if (strength <= 0) {
+            System.out.println("Некорректный ввод");
+        } else {
+            this.strength = strength;
+        }
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    void setArmor(int armor) {
+        if (armor <= 0) {
+            System.out.println("Некорректный ввод");
+        } else {
+            this.armor = armor;
+        }
+    }
+
+    void drift() {
+        System.out.println("Вы вошли в дрифт");
+    }
+
+    void move() {
+        System.out.println("Вы прошли 5 шагов");
+    }
+
+    void attack() {
+        System.out.println("Вы атакуете");
+    }
+}
---
+++ lesson_2/JaegerTest.java
@@ -0,0 +1,36 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger gipsyDanger = new Jaeger();
+        gipsyDanger.setModelName("Gipsy Danger");
+        gipsyDanger.setMark("Mark-3");
+        gipsyDanger.setOrigin("United States of America");
+        gipsyDanger.setHeight(79.25f);
+        gipsyDanger.setWeight(1.980f);
+        gipsyDanger.setSpeed(7);
+        gipsyDanger.setStrength(8);
+        gipsyDanger.setArmor(6);
+        System.out.println("ModelName: " + gipsyDanger.getModelName());
+        System.out.println("Mark: " + gipsyDanger.getMark());
+        System.out.println("Origin: " + gipsyDanger.getOrigin());
+        System.out.println("Height: " + gipsyDanger.getHeight());
+        System.out.println("Weight: " + gipsyDanger.getWeight());
+        System.out.println("Speed: " + gipsyDanger.getSpeed());
+        System.out.println("Strength: " + gipsyDanger.getStrength());
+        System.out.println("Armor: " + gipsyDanger.getArmor());
+        gipsyDanger.drift();
+        gipsyDanger.move();
+        gipsyDanger.attack();
+        Jaeger strikerEureka = new Jaeger("Striker Eureka", "Mark-5", "Australia", 76.2f, 1.850f, 10, 10, 9);
+        System.out.println("\nModelName: " + strikerEureka.getModelName());
+        System.out.println("Mark: " + strikerEureka.getMark());
+        System.out.println("Origin: " + strikerEureka.getOrigin());
+        System.out.println("Height: " + strikerEureka.getHeight());
+        System.out.println("Weight: " + strikerEureka.getWeight());
+        System.out.println("Speed: " + strikerEureka.getSpeed());
+        System.out.println("Strength: " + strikerEureka.getStrength());
+        System.out.println("Armor: " + strikerEureka.getArmor());
+        strikerEureka.drift();
+        strikerEureka.move();
+        strikerEureka.attack();
+    }
+}
